f0ec668a213a72aa6b312c554a7095fb24b436b6
euseanwoon2016 <euseanwoon2016@github.com>
2020-04-20 16:12:02 +0800

    Added Disk Scheduling

--- src/javatesting/OSAnswerer.java
+++ src/javatesting/OSAnswerer.java
@@ -52,7 +52,7 @@ public class OSAnswerer {
         ArrayList<ProcInfo> processes = new ArrayList<ProcInfo>();
         ArrayList<ProcRecord> processRecords = new ArrayList<ProcRecord>();
 
-        System.out.println("Enter the time slice:");
+        System.out.println("Enter the time slice.");
         int timeSlice = Integer.parseInt(scn.nextLine());
 
         System.out.println("Enter process burst times seperated by commas.");
@@ -131,7 +131,7 @@ public class OSAnswerer {
             totalTurnaroundTime += wt + bt;
         }
         System.out.println("Tt(Average) = " + totalTurnaroundTime / turnAroundTimes.size() + " ms\n");
-        
+
         System.out.println("Press enter to return to menu.");
         scn.nextLine();
     }
@@ -199,13 +199,13 @@ public class OSAnswerer {
             totalTurnaroundTime += wt + bt;
         }
         System.out.println("Tt(Average) = " + totalTurnaroundTime / turnAroundTimes.size() + " ms\n");
-        
+
         System.out.println("Press enter to return to menu.");
         scn.nextLine();
     }
 
     static void FIFO() {
-        System.out.println("Please enter the number of page frames");
+        System.out.println("Please enter the number of page frames.");
         int cap = Integer.parseInt(scn.nextLine());
         Queue<String> pageFrames = new ArrayBlockingQueue<String>(cap);
 
@@ -235,7 +235,7 @@ public class OSAnswerer {
             System.out.println(pageFrames);
         }
         System.out.println("Total Page Faults: " + counter + "\n");
-        
+
         System.out.println("Press enter to return to menu.");
         scn.nextLine();
     }
@@ -274,7 +274,170 @@ public class OSAnswerer {
 
         }
         System.out.println("Total Page Faults: " + counter + "\n");
-        
+
+        System.out.println("Press enter to return to menu.");
+        scn.nextLine();
+    }
+
+    static void diskSchedSSTF() {
+        ArrayList<Integer> requestList = new ArrayList<>();
+        ArrayList<Integer> solvedList = new ArrayList<>();
+
+        System.out.println("Enter the request queue seperated by commas.");
+        String req = scn.nextLine();
+        for (String s : req.split(",")) {
+            requestList.add(Integer.parseInt(s));
+        }
+
+        System.out.println("Enter start position of head.");
+        Integer startPos = Integer.parseInt(scn.nextLine());
+
+        while (true) {
+            if (requestList.isEmpty()) {
+                break;
+            }
+
+            //               v         d
+            int[] bestReq = {startPos, 9999};
+            // loop thru list
+            for (Integer currReq : requestList) {
+                int currDiff = Math.abs(startPos - currReq);
+
+                if (currDiff == 0) {
+                    continue;
+                }
+
+                if (currDiff < bestReq[1]) {
+                    bestReq[0] = currReq;
+                    bestReq[1] = currDiff;
+                }
+            }
+            startPos = bestReq[0];
+            requestList.remove(startPos);
+            solvedList.add(startPos);
+        }
+
+        System.out.println(solvedList);
+
+        System.out.println("Press enter to return to menu.");
+        scn.nextLine();
+    }
+
+    static void diskSchedSCANLOOK() {
+        ArrayList<Integer> requestList = new ArrayList<>();
+        ArrayList<Integer> solvedList = new ArrayList<>();
+
+        System.out.println("Enter the minimum value of the SCAN.");
+        Integer minVal = Integer.parseInt(scn.nextLine());
+
+        System.out.println("Enter the maximum value of the SCAN.");
+        Integer maxVal = Integer.parseInt(scn.nextLine());
+
+        System.out.println("Enter the request queue seperated by commas.");
+        String req = scn.nextLine();
+        for (String s : req.split(",")) {
+            requestList.add(Integer.parseInt(s));
+        }
+
+        System.out.println("Enter start position of head.");
+        Integer startPos = Integer.parseInt(scn.nextLine());
+
+        System.out.println("Enter direction of SCAN (L, R).");
+        String scanDirection = scn.nextLine();
+
+        while (true) {
+            if (requestList.isEmpty()) {
+                break;
+            }
+
+            if (scanDirection.equalsIgnoreCase("L")) {
+                for (int i = startPos; i > minVal; i--) {
+                    if (requestList.indexOf(i) != -1) {
+                        requestList.remove((Integer) i);
+                        solvedList.add(i);
+                    }
+                }
+                scanDirection = "R";
+            }
+            if (scanDirection.equalsIgnoreCase("R")) {
+                for (int i = startPos; i < maxVal; i++) {
+                    if (requestList.indexOf(i) != -1) {
+                        requestList.remove((Integer) i);
+                        solvedList.add(i);
+                    }
+                }
+                scanDirection = "L";
+            }
+        }
+
+        System.out.println(solvedList);
+
+        System.out.println("Press enter to return to menu.");
+        scn.nextLine();
+    }
+
+    static void diskSchedCSCANCLOOK() {
+        ArrayList<Integer> requestList = new ArrayList<>();
+        ArrayList<Integer> solvedList = new ArrayList<>();
+
+        System.out.println("Enter the minimum value of the CSCAN.");
+        Integer minVal = Integer.parseInt(scn.nextLine());
+
+        System.out.println("Enter the maximum value of the CSCAN.");
+        Integer maxVal = Integer.parseInt(scn.nextLine());
+
+        System.out.println("Enter the request queue seperated by commas.");
+        String req = scn.nextLine();
+        for (String s : req.split(",")) {
+            requestList.add(Integer.parseInt(s));
+        }
+
+        System.out.println("Enter start position of head.");
+        Integer startPos = Integer.parseInt(scn.nextLine());
+
+        System.out.println("Enter direction of CSCAN (L, R).");
+        String scanDirection = scn.nextLine();
+
+        while (true) {
+            if (requestList.isEmpty()) {
+                break;
+            }
+            if (scanDirection.equalsIgnoreCase("L")) {
+                Integer currReq = startPos - 1;
+                while (true) {
+                    if (currReq < minVal) {
+                        currReq = maxVal;
+                    }
+                    if (currReq == startPos) {
+                        break;
+                    }
+                    if (requestList.indexOf(currReq) != -1) {
+                        requestList.remove(currReq);
+                        solvedList.add(currReq);
+                    }
+                    currReq--;
+                }
+            }
+            if (scanDirection.equalsIgnoreCase("R")) {
+                Integer currReq = startPos + 1;
+                while (true) {
+                    if (currReq > maxVal) {
+                        currReq = minVal;
+                    }
+                    if (currReq == startPos) {
+                        break;
+                    }
+                    if (requestList.indexOf(currReq) != -1) {
+                        requestList.remove(currReq);
+                        solvedList.add(currReq);
+                    }
+                    currReq++;
+                }
+            }
+        }
+
+        System.out.println(solvedList);
+
         System.out.println("Press enter to return to menu.");
         scn.nextLine();
     }
@@ -282,18 +445,20 @@ public class OSAnswerer {
     public static void main(String[] args) {
         while (isRunning) {
             System.out.println(
-                    "   ____  _____    ___    _   ________       ____________  __________ \n"
-                    + "  / __ \\/ ___/   /   |  / | / / ___/ |     / / ____/ __ \\/ ____/ __ \\\n"
-                    + " / / / /\\__ \\   / /| | /  |/ /\\__ \\| | /| / / __/ / /_/ / __/ / /_/ /\n"
-                    + "/ /_/ /___/ /  / ___ |/ /|  /___/ /| |/ |/ / /___/ _, _/ /___/ _, _/ \n"
-                    + "\\____//____/  /_/  |_/_/ |_//____/ |__/|__/_____/_/ |_/_____/_/ |_|  \n"
-                    + "                                                                     \n"
+                      "   ____  _____    ___    _   ________       ____________  __________ \n"
+                    + "  / __ \\/ ___/   /   |  / | / / ___/ |     / / ____/ __ \\/ ____/ __ \\  _   _____ \n"
+                    + " / / / /\\__ \\   / /| | /  |/ /\\__ \\| | /| / / __/ / /_/ / __/ / /_/ / | | / /_  |\n"
+                    + "/ /_/ /___/ /  / ___ |/ /|  /___/ /| |/ |/ / /___/ _, _/ /___/ _, _/  | |/ / __/ \n"
+                    + "\\____//____/  /_/  |_/_/ |_//____/ |__/|__/_____/_/ |_/_____/_/ |_|   |___/____/ \n"
                     + "Choose a mode :\n"
                     + "[1] PCM (Round Robin)\n"
                     + "[2] PCM (FCFS)\n"
                     + "[3] Page Replacement (FIFO)\n"
                     + "[4] Page Replacement (LRU)\n"
-                    + "[5] Exit");
+                    + "[5] Disk Scheduling (SSTF)\n"
+                    + "[6] Disk Scheduling (SCAN/LOOK)\n"
+                    + "[7] Disk Scheduling (C-SCAN/C-LOOK)\n"
+                    + "[0] Exit");
             int choice = Integer.parseInt(scn.nextLine());
             switch (choice) {
                 case 1:
@@ -309,6 +474,15 @@ public class OSAnswerer {
                     LRU();
                     break;
                 case 5:
+                    diskSchedSSTF();
+                    break;
+                case 6:
+                    diskSchedSCANLOOK();
+                    break;
+                case 7:
+                    diskSchedCSCANCLOOK();
+                    break;
+                case 0:
                     isRunning = false;
                     break;
                 default:
